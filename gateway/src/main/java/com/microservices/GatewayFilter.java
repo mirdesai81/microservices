aaecfb0b117ab731dffbf105ccaf063887293f5a
Mihir.Desai <mirdesai81@gmail.com>
2018-03-11 09:46:33 -0700

    Gateway API using Zuul

---
+++ gateway/src/main/java/com/microservices/GatewayFilter.java
@@ -0,0 +1,41 @@
+package com.microservices;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by Saloni on 3/10/2018.
+ */
+public class GatewayFilter extends ZuulFilter {
+    private static Logger log = LogManager.getLogger(GatewayFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+
+        return null;
+    }
+}
